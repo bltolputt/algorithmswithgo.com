40640d91933069a41373ba49b2fbfbc2ba31c41b
Benjamin Tolputt <benjamin.tolputt@gmail.com>
2020-03-08 15:27:24 +0000

    gcd using euclid algorithm

--- module01/gcd.go
+++ module01/gcd.go
@@ -1,5 +1,16 @@
 package module01
 
 func GCD(a, b int) int {
-	return 0
+
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+
+	// Recursive
+	//if b == 0 {
+	//	return a
+	//}
+	//a, b = b, a%b
+	//return GCD(a, b)
 }
